aa36db3ccd307b6a5a396e8112678f0af42e3918
Dan Luo <luod@usc.edu>
2016-09-15 15:49:14 -0700

    UCS

--- homework1/homework.py
+++ homework1/homework.py
@@ -70,6 +70,7 @@ def dfs(start, end, graph):
     if start == end:
         return [start]
     while queue:
+        print queue
         path = queue.pop(0)
         current = path[-1]
         expand = False
@@ -113,17 +114,145 @@ def dfs(start, end, graph):
     return False
 
 
-def ucs(index):
-    #print index
+def ucs_perpare(index, algorithm):
     start = input[index + 1]
     end = input[index + 2]
+    graph = {}
     live_num = int(input[index + 3])
     for i in range(0, live_num):
-        print input[index + i + 4]
+        live = input[index + i + 4].split(" ")
+        edge = []
+        edge.append(live[1])
+        edge.append(int(live[2]))
+        if live[0] in graph.keys():
+            graph[live[0]].append(edge)
+        else:
+            graph[live[0]] = [edge]
+    print graph
     sat_num = int(input[index + live_num + 4])
-    #for i in range(0,sat_num):
-    #    print input[index + live_num + i + 5]
+    if algorithm == 'UCS':
+        ucs(graph, start, end)
     return index + live_num + sat_num + 5
+
+
+def ucs(graph, start, end):
+    open = [[0, start, 0, 0, None]] # num, state, depth, cost, parent
+    num = 0 #initial
+    closed = []
+    while open:
+        current = open.pop(0)
+        if current[1] == end:
+
+            path = generate_path(open, closed, current)
+            return [path, current[3]]
+
+        if current[1] in graph.keys():
+            children = graph[current[1]]
+            while children:
+                child = children.pop(0)
+                exist_in_open = False
+                node_in_open_cost = -1
+                node_in_open_index = -1
+                exist_in_closed = False
+                node_in_closed_cost = -1
+                node_in_closed_index = -1
+                for state in open:
+                    if state[1] == child[0]:
+                        if exist_in_open:
+                            if node_in_open_cost > state[3]:
+                                node_in_open_cost = state[3]
+                                node_in_open_index = open.index(state)
+                        else:
+                            exist_in_open = True
+                            node_in_open_cost = state[3]
+                            node_in_open_index = open.index(state)
+
+                for state in closed:
+                    if state[1] == child[0]:
+                        if exist_in_closed:
+                            if node_in_closed_cost > state[3]:
+                                node_in_closed_cost = state[3]
+                                node_in_closed_index = closed.index(state)
+                        else:
+                            exist_in_closed = True
+                            node_in_closed_cost = state[3]
+                            node_in_closed_index = closed.index(state)
+
+                if not exist_in_open and not exist_in_closed:
+                    # add child
+                    inserted = False
+                    num += 1
+                    node = [num, child[0], current[2] + 1, current[3] + child[1], current[0]]
+                    for state in open:
+                        if state[3] > current[3] + child[1]:
+                            open.insert(open.index(state), node)
+                            inserted = True
+                            break
+
+                    if not inserted:
+                        open.insert(len(open), node)
+
+                elif exist_in_open:
+                    if current[3] + child[1] < node_in_open_cost:
+                        open.pop(node_in_open_index)
+                        inserted = False
+                        num += 1
+                        node = [num, child[0], current[2] + 1, current[3] + child[1], current[0]]
+                        for state in open:
+                            if state[3] > current[3] + child[1]:
+                                open.insert(open.index(state), node)
+                                inserted = True
+                                break
+
+                        if not inserted:
+                            open.insert(len(open), node)
+
+                elif exist_in_closed:
+                    if current[3] + child[1] < node_in_closed_cost:
+                        closed.pop(node_in_closed_index)
+                        inserted = False
+                        num += 1
+                        node = [num, child[0], current[2] + 1, current[3] + child[1], current[0]]
+                        for state in open:
+                            if state[3] > current[3] + child[1]:
+                                open.insert(open.index(state), node)
+                                inserted = True
+                                break
+
+                        if not inserted:
+                            open.insert(len(open), node)
+
+        closed.append(current)
+    return False
+
+
+def generate_path(open, closed, end):
+    path = []
+    current = end
+    record = [end[1], end[3]]
+    path.append(record)
+
+    while current[4] != None:
+        parent = current[4]
+        in_open = False
+        for node in open:
+            if node[0] == parent:
+                record = [node[1], node[3]]
+                path.insert(0, record)
+                current = node
+                in_open = True
+                break
+        if not in_open:
+            for node in closed:
+                if node[0] == parent:
+                    record = [node[1], node[3]]
+                    path.insert(0, record)
+                    current = node
+                    break
+
+    for point in path:
+        out.write(point[0] + ' ' + str(point[1]) + '\n')
+
 
 
 def a_search(index):
@@ -144,7 +273,8 @@ for i in range(0,len(input)):
     if input[i] == 'BFS' or input[i] == 'DFS':
         i = bfs_dfs_perpare(i, input[i])
     elif input[i] == 'UCS':
-        ucs(i)
+        ucs_perpare(i, input[i])
+        #ucs(i)
     elif input[i] == 'A*':
         a_search(i)
 out.close()
